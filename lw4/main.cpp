2aad51f59fe1a358e67a3170466dcf1c1f525793
kirill.borisov <kirill.borisov@ispring.com>
2023-12-14 02:35:39 +0300

    Закончил 4

--- lw4/main.cpp
+++ lw4/main.cpp
@@ -19,8 +19,9 @@
 #include <string>
 #include <algorithm>
 #include <vector>
-#include <queue>
-#include <stack>
+#include <locale>
+#include <codecvt>
+#include <cstdlib>
 
 using namespace std;
 
@@ -31,127 +32,38 @@ struct EffectNode
 	int output{};
 };
 
-// Рекурсивная функция для вставки элемента в конец заданного stack
-void insertAtBottom(stack<int>& s, int item)
-{
-	// базовый случай: если stack пуст, вставить данный элемент в конец
-	if (s.empty())
-	{
-		s.push(item);
-		return;
-	}
-
-	// Извлечь все элементы из стека и сохранить их в стеке вызовов
-	int top = s.top();
-	s.pop();
-	insertAtBottom(s, item);
-
-	// После развертывания рекурсии помещаем каждый элемент в стек вызовов
-	// на вершине stack
-	s.push(top);
-}
-
-// Рекурсивная функция для обращения заданного stack
-void reverseStack(stack<int>& s)
-{
-	// базовый случай: stack пуст
-	if (s.empty())
-	{
-		return;
-	}
-
-	// Извлечь все элементы из стека и сохранить их в стеке вызовов
-	int item = s.top();
-	s.pop();
-	reverseStack(s);
-
-	// После развертывания рекурсии вставляем каждый элемент в стек вызовов
-	// внизу stack
-	insertAtBottom(s, item);
-}
-
-stack<int> outputChain(const std::vector<std::vector<int>>& mockGraph, int node, int distance[], int start)
-{
-	std::stack<int> stack;
-	if (node == start)
-	{
-		stack.push(node);
-		return stack;
-	}
-	for (int i = 0; i < mockGraph.size(); ++i)
-	{
-		if (i == node) continue;
-		if (mockGraph[i][node] != 0 && distance[i] == distance[node] - 1)
-		{
-			stack.push(node);
-			if (i != start)
-			{
-				std::stack<int> tmpStack;
-				tmpStack = outputChain(mockGraph, i, distance, start);
-				reverseStack(tmpStack);
-				while (!tmpStack.empty())
-				{
-					stack.push(tmpStack.top());
-					tmpStack.pop();
-				}
-			}
-			else
-			{
-				stack.push(i);
-			}
-			break;
-		}
-	}
-
-	return stack;
-}
-
-void outputReactionChains(
-	const ofstream& ofs,
-	const std::vector<std::vector<std::vector<int>>>& mockGraph,
+void searchWays(
+	std::vector<std::vector<std::vector<int>>>& graph,
 	int input,
-	const int output,
-	std::vector<EffectNode> effectNodes
+	int output,
+	std::vector<std::vector<int>>& ways,
+	std::vector<int>& way,
+	std::vector<EffectNode>& effectNodes
 )
 {
-	std::queue<int> q;
-	int distance[mockGraph.size()];
-	for (auto& dist: distance) dist = -1;
-
-	int chains[mockGraph.size()];
-	for (auto& chain : chains) chain = -1;
-
-	q.push(input);
-	distance[input] = 0;
-	while (!q.empty())
+	way.push_back(input);
+	if (input == output)
 	{
-		int v = q.front();
-		if (distance[v] + 1 == 5)
-		{
-			break;
-		}
-		q.pop();
-		for (int i = 0; i < mockGraph[v].size(); i++)
-		{
-			if (i == v) continue;
-			if (!mockGraph[v][i].empty())
-			{
-				q.push(i);
-				distance[i] = distance[v] + 1;
-				chains[i] = v;
-			}
-		}
+		ways.push_back(way);
+		way.pop_back();
+		return;
 	}
 
-//	std::sort(chains.begin(), chains.end());
-	for (auto chain: chains)
+	if (way.size() >= 4)
 	{
-		if (chain >= 0)
-		{
-			std::cout << chain << " <== ";
-		}
+		way.pop_back();
+		return;
 	}
-	std::cout << std::endl;
+
+	for (int i = 0; i < graph[input].size(); i++)
+	{
+		if (graph[input][i].empty() || std::find(way.begin(), way.end(), i) != way.end())
+		{
+			continue;
+		}
+		searchWays(graph, i, output, ways, way, effectNodes);
+	}
+	way.pop_back();
 }
 
 std::vector<std::vector<std::vector<int>>>
@@ -172,7 +84,8 @@ createGraph(std::vector<string>& quantityList, std::vector<EffectNode>& effectNo
 
 			for (int k = 0; k < effectNodes.size(); k++)
 			{
-				if (std::find(effectNodes[k].input.begin(), effectNodes[k].input.end(), i) != effectNodes[k].input.end()
+				if (std::find(effectNodes[k].input.begin(), effectNodes[k].input.end(), i) !=
+					effectNodes[k].input.end()
 					&& effectNodes[k].output == j)
 				{
 					effects.push_back(k);
@@ -186,6 +99,7 @@ createGraph(std::vector<string>& quantityList, std::vector<EffectNode>& effectNo
 
 	return graph;
 }
+
 
 std::vector<EffectNode> parseEffectsFile(ifstream& ifs)
 {
@@ -208,10 +122,10 @@ std::vector<EffectNode> parseEffectsFile(ifstream& ifs)
 					switch (flag)
 					{
 					case 1:
-						node.input.push_back(num);
+						node.input.push_back(num - 1);
 						break;
 					case 2:
-						node.output = num;
+						node.output = num - 1;
 						break;
 					default:
 						break;
@@ -228,10 +142,10 @@ std::vector<EffectNode> parseEffectsFile(ifstream& ifs)
 				switch (flag)
 				{
 				case 1:
-					node.input.push_back(num);
+					node.input.push_back(num - 1);
 					break;
 				case 2:
-					node.output = num;
+					node.output = num - 1;
 					break;
 				}
 				inp = {};
@@ -286,6 +200,49 @@ std::vector<std::string> parsePhysicalQuantityFile(ifstream& ifs)
 	return physicalQuantities;
 }
 
+void outputWays(std::vector<std::vector<int>>& ways, wofstream& wofs, std::vector<std::string>& quantityList, std::vector<EffectNode> effectNodes)
+{
+	std::sort(ways.begin(), ways.end(), [](auto &way1, auto &way2){
+		return way1.size() < way2.size();
+	});
+	for (auto& way : ways)
+	{
+		auto isFirst = true;
+		for (auto quantity : way)
+		{
+			if (!isFirst)
+			{
+				wofs << L" ==> ";
+			}
+			isFirst = false;
+			std::wstring ws(quantityList[quantity + 1].size(), L' ');
+			ws.resize(std::mbstowcs(&ws[0], quantityList[quantity + 1].c_str(), quantityList[quantity + 1].size()));
+			wofs << ws;
+		}
+		wofs << endl;
+		for (int i = 0; i < way.size() - 1; i++)
+		{
+			wofs << L"Звено ";
+			std::wstring ws1(quantityList[way[i] + 1].size(), L' ');
+			ws1.resize(std::mbstowcs(&ws1[0], quantityList[way[i] + 1].c_str(), quantityList[way[i] + 1].size()));
+			std::wstring ws2(quantityList[way[i + 1] + 1].size(), L' ');
+			ws2.resize(std::mbstowcs(&ws2[0], quantityList[way[i + 1] + 1].c_str(), quantityList[way[i + 1] + 1].size()));
+			wofs << to_wstring(i + 1) << ": " << ws1 << " ==> " << ws2 << endl;
+			for (auto& effect : effectNodes)
+			{
+				if (find(effect.input.begin(), effect.input.end(), way[i]) != effect.input.end()
+					&& effect.output == way[i + 1])
+				{
+					std::wstring ws3(effect.name.size(), L' ');
+					ws3.resize(std::mbstowcs(&ws3[0], effect.name.c_str(), effect.name.size()));
+					wofs << ws3 << endl;
+				}
+			}
+		}
+		wofs << endl;
+	}
+}
+
 int main()
 {
 	setlocale(0, "");
@@ -304,7 +261,6 @@ int main()
 	}
 
 	std::vector<std::string> quantityList = parsePhysicalQuantityFile(ifsPower);
-	cout << quantityList[0];
 
 	std::wcout << L"Введите название входного файла с физическими величинами: ";
 	string inFileNameEffects;
@@ -313,28 +269,33 @@ int main()
 	if (!ifsEffects)
 	{
 		std::wstring wsTmp(inFileNameEffects.begin(), inFileNameEffects.end());
-		wcerr << TEXT("Не получилось открыть файл: ") << wsTmp << endl;
+		wcerr << L"Не получилось открыть файл: " << wsTmp << endl;
 		exit(1);
 	}
 
 	std::vector<EffectNode> effectNodes = parseEffectsFile(ifsEffects);
 
-	const std::vector<std::vector<std::vector<int>>> graph = createGraph(quantityList, effectNodes);
+	std::vector<std::vector<std::vector<int>>> graph = createGraph(quantityList, effectNodes);
 
-	int input = 18;
-	int output = 10;
+	int input = 0;
+	int output = 0;
 
-//	std::wcout << L"Введите input: ";
-//	cin >> input;
-//	std::wcout << L"Введите output: ";
-//	cin >> output;
+	std::wcout << L"Введите input: ";
+	cin >> input;
+	std::wcout << L"Введите output: ";
+	cin >> output;
 
 	wcout << L"Введите название выходного файла: ";
 	string outFileName;
 	cin >> outFileName;
-	ofstream ofs;
+	wofstream ofs(outFileName);
+	std::locale loc(std::locale::classic(), new std::codecvt_utf8<wchar_t>);
+	ofs.imbue(loc);
 
-	outputReactionChains(ofs, graph, input, output, effectNodes);
+	std::vector<std::vector<int>> ways{};
+	std::vector<int> way{};
+	searchWays(graph, input, output, ways, way, effectNodes);
+	outputWays(ways, ofs, quantityList, effectNodes);
 
 	ifsPower.close();
 	ifsEffects.close();
